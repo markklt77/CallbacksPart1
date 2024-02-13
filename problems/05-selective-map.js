41567b3823d66777432954eeed8cf9496efc7610
markklt77 <markklt77@gmail.com>
2024-02-12 20:24:50 -0800

    5th

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -37,7 +37,15 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 *******************************************************************************/
 
 function selectiveMap(array, selector, mapper) {
-    // Your code here 
+    let arr = [];
+    for (let el of array) {
+        if (selector(el)) {
+            arr.push(mapper(el));
+        }else {
+            arr.push(el);
+        }
+    }
+    return arr;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
