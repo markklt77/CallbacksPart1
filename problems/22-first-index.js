a2ee4a7ef54675ed35d23610728f219b79d8e96f
sause668 <soulsitescoding@zohomail.com>
2024-02-13 22:42:44 -0500

    p22

--- problems/22-first-index.js
+++ problems/22-first-index.js
@@ -1,3 +1,5 @@
+
+
 /*******************************************************************************
 Write a function `firstIndex` that accepts an array and a callback as arguments.
 The function should return the index of the first element of the array that
@@ -23,7 +25,13 @@ console.log(result3); // -1
 *******************************************************************************/
 
 function firstIndex(array, cb) {
-  // Your code here 
+  for (let i = 0; i < array.length; i++) {
+    if (cb(array[i])) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
